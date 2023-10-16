13588b2f595f923dab02b2d6f169720f6da3d830
Oyewale <oyewalefavour424@gmail.com>
2023-10-16 03:52:46 +0100

    added filters

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:simple_shoe_app/pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +12,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow.shade300),
+        inputDecorationTheme:
+            const InputDecorationTheme(prefixIconColor: Colors.grey),
+      ),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +9,81 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> filters = const [
+    "All",
+    "Addidas",
+    "Reebok",
+    "Balenciaga",
+    "Nike"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello world'),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Shoes \nCollections",
+                    style: GoogleFonts.epilogue(
+                      textStyle: const TextStyle(
+                          fontSize: 23, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 21,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: GoogleFonts.epilogue(
+                            textStyle: const TextStyle(fontSize: 18)),
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          size: 28,
+                        ),
+                        border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                            )),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    final filter = filters[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Chip(
+                        padding: EdgeInsets.all(10),
+                        label: Text(
+                          filter,
+                          style: GoogleFonts.epilogue(
+                              textStyle: TextStyle(
+                            fontSize: 16,
+                          )),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: filters.length,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
